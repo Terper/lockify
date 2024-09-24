3c5e44743333dbf0574dfe0bbe1e0676f0bd1b07
arcaadser <hellenha@arcada.fi>
2024-09-24 18:35:22 +0300

    Feat: Add Vue-components. Init Login. Join gitignores.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Login from './components/Login.vue'
+import NowPlaying from './components/NowPlaying.vue'
+import Playlist from './components/Playlist.vue'
+import MainMenu from './components/MainMenu.vue'
 </script>
 
 <template>
@@ -8,12 +10,17 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="greetings">
+    <h1 class="green">Lockify</h1>
+      </div>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+      <Login></Login>
+    <NowPlaying></NowPlaying>
+      <Playlist></Playlist>
+      <MainMenu></MainMenu>
   </main>
 </template>
 
---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="login-form" v-if="visible">
+        <form>
+            <label for="username">Användarnamn:</label>
+            <input id="username" type="text" v-model="username">
+            <label for="password">Lösenord:</label>
+            <input id="password" type="password" v-model="password">
+            <button type="submit" @click.prevent="process">Logga in</button>
+        </form>
+    </div>
+    <div id="logout" v-else v-cloak><p @click="process">Logga ut</p></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            visible: true,
+            username: '',
+            password: ''
+        }
+    }, methods: {
+        process: function (event) {
+            this.visible = !this.visible 
+        }
+    }
+}
+</script>
+
+<style>
+[v-cloak] { display: none; }
+</style>
